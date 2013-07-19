3e84defe8135d7fc60a0b6e7147b67273a172ef8
Roger <Roger.Mendez.R@gamil.com>
2013-07-19 02:09:46 -0400

    sdlkfj

--- sapec/wsgi.py
+++ sapec/wsgi.py
@@ -26,7 +26,6 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
-
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
