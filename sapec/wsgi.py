36161a003ec0ea19001b8e3b299d28e0d95d7187
RogerMendez <Roger.Mendez.R@gmail.com>
2014-05-17 17:53:18 -0400

    Heroku init

--- sapec/wsgi.py
+++ sapec/wsgi.py
@@ -27,10 +27,10 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "sapec.settings"
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+#from django.core.wsgi import get_wsgi_application
+#from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
